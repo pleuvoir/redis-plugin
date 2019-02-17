cba6deafae9a518f17d58fd2cccef06be82e8fc0
pleuvoir <pleuvior@foxmail.com>
2019-02-17 14:57:14 +0800

    update version v1.1.1

--- version.bat
+++ version.bat
@@ -13,7 +13,7 @@
 rem  Please execute command in local directory.
 
 call mvn -N versions:update-child-modules
-call mvn versions:set -DnewVersion=1.1.0
+call mvn versions:set -DnewVersion=1.1.1
 call mvn versions:commit
 
 pause
